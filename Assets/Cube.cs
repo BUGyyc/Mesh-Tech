40be551e5bb60a4c9f5666db450445d7213f66f8
delevin <846353427@qq.com>
2020-05-23 17:37:26 +0800

    cube

---
+++ Assets/Cube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Cube : MonoBehaviour
+{
+    public int xSize, ySize, zSize;
+    private Mesh mesh;
+    private Vector3[] vertices;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
